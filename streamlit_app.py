ccf3899860704406063cbd9ecd1392fb43bd22e8
Yash jain <55264027+YashJain2@users.noreply.github.com>
2021-11-22 18:16:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.title('Compare ROI Of Stocks')
 
 # creating a tuple
-tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD','TTM','RELIANCE.NS','TATAMOTORS.NS')
+tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD','TTM','RELIANCE.NS','TATAMOTORS.NS','SBIN')
 
 #dropdown lists of stocks
 dropdown = st.multiselect('Choose your stocks', tickers)
